700039564f0738c764b9e46a88e349b54a763f08
Sayed Nahid <nahid15-3849@diu.edu.bd>
2024-11-17 18:19:54 +0600

    serializers.py file is ready

---
+++ quiz/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Student, Question, QuestionOption
+
+class Student(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['id', 'username', 'score', 'status']
+
+class QuestionOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuestionOption
+        fields = ['id', 'option', 'is_correct']
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['id', 'question', 'options']
