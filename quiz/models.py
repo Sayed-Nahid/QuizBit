85d3af56b338b5d04405832a73098c846b31d214
Sayed Nahid <nahid15-3849@diu.edu.bd>
2024-11-17 14:15:28 +0600

    Working on models.py file and Student Table is ready

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    score = models.IntegerField(default=0)
+    status = models.CharField(max_length=20, default="active")
+    def __str__(self):
+        return self.username
